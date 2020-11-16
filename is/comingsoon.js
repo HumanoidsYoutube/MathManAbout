ef0a68b0554f34167b7e6f111ef58b0685522592
Jack Mathias <73205188+HumanoidsYoutube@users.noreply.github.com>
2020-11-16 11:45:16 -0600

    Update junk w/ GitHub Desktop
    Yes

--- is/comingsoon.js
+++ is/comingsoon.js
@@ -1,10 +1,53 @@
-var title = document.getElementById("title");
 var head = document.getElementById("h1");
 var sub = document.getElementById("h2");
+var link = document.getElementById("link");
+
+function load() {
+    var cookie = checkCookie();
+    if(cookie === "yes") {
+    location.replace("")
+        setCookie("newload", "clear", )
+    }
+}
 
 function back() {
     head.style.color = "white";
     sub.style.color = 'white';
+    link.innerHTML = '';
     document.getElementById("b").style.backgroundColor = 'white';
-    setTimeout(function(){window.history.back();}, 1000)
+    location.replace("#h1")
+    setTimeout(function(){location.replace("javascript:window.history.back()");}, 1000)
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user = getCookie("newload");
+    if (user != "" && user != null && user != "clear") {
+      setCookie("newload", "yes", 0.01);
+      return "yes";
+    }else{
+      return "no";
+    }
+  }
 }
